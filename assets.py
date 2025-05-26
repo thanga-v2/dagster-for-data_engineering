da235f162d7c4c1f9ac6cd78136ce445da9aca89
thanga-v2 <thangarajm@icloud.com>
2025-05-26 14:31:11 +0530

    dagster, asset and def object

---
+++ assets.py
@@ -0,0 +1,23 @@
+import dagster as dg
+
+print(dg)
+
+@dg.asset
+def simple(context: dg.AssetExecutionContext):
+    context.log.info("A simple asset")
+
+
+
+@dg.asset(deps=[simple])
+def second(context: dg.AssetExecutionContext):
+    context.log.info("second asset")    
+
+
+
+@dg.asset(deps=[second])
+def third(context: dg.AssetExecutionContext):
+    context.log.info("third asset")  
+
+# dagster definitions  
+# 
+dagsterdefs = dg.Definitions(assets=[simple, second, third])        
